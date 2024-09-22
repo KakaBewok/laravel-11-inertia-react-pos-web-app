e990ac8a6276af0c5e29677334051ee02731d8a8
noprizal <noprizal@voltras.co.id>
2024-09-22 15:06:24 +0700

    init

---
+++ database/migrations/2024_09_22_060338_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained(
+                table: 'categories',
+                indexName: 'products_category_id'
+            )->onDelete('cascade');
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->string('slug');
+            $table->unsignedBigInteger('price');
+            $table->string('unit');
+            $table->integer('stock_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //create permissions
+        $permissions = [
+            'manage categories',
+            'manage products',
+            'manage photos',
+            'manage payment_methods',
+            'manage expenses',
+            'manage orders'
+        ];
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(
+                [
+                    'name' => $permission
+                ]
+            );
+        }
+
+        // create roles
+        $adminRole = Role::create(['name' => 'admin']);
+        $superAdminRole = Role::create(['name' => 'super_admin']);
+
+        // give permission to roles
+        $adminRole->syncPermissions('manage orders');
+
+        //create super_admin user
+        $superAdminUser = User::create([
+            'name' => 'Super Admin',
+            'email' => 'super.admin@gmail.com',
+            'password' => bcrypt('superadmin')
+        ]);
+        $superAdminUser->assignRole($superAdminRole);
+
+        //create admin user
+        $adminUser = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('adminadmin')
+        ]);
+        $adminUser->assignRole($adminRole);
+    }
+}
