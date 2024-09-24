1c0cff860e257779ea05f0051cf8786c06114c96
noprizal <noprizal@voltras.co.id>
2024-09-25 00:10:49 +0700

    fixing interface

--- app/Interfaces/CrudRepository.php
+++ app/Interfaces/CrudRepository.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\View;
 
 interface CrudRepository
 {
@@ -11,24 +10,9 @@ interface CrudRepository
     public function all(): Collection;
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create(): View;
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(array $data);
-
-    /**
-     * Display the specified resource.
-     */
-    public function detail(int $id): View;
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(int $id): View;
 
     /**
      * Update the specified resource in storage.
