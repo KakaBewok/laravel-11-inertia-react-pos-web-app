e602539603ca48b36a0d4c559be4d1002929f24c
Noprizal <noprizal@voltras.co.id>
2024-09-27 18:01:29 +0700

    fixing

--- resources/js/Layouts/DefaultLayout.tsx
+++ resources/js/Layouts/DefaultLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import Header from "../Components/Header/index";
 import Sidebar from "../Components/Sidebar/index";
 
@@ -7,35 +7,25 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
 
     return (
         <div className="dark:bg-boxdark-2 dark:text-bodydark">
-            {/* <!-- ===== Page Wrapper Start ===== --> */}
             <div className="flex h-screen overflow-hidden">
-                {/* <!-- ===== Sidebar Start ===== --> */}
                 <Sidebar
                     sidebarOpen={sidebarOpen}
                     setSidebarOpen={setSidebarOpen}
                 />
-                {/* <!-- ===== Sidebar End ===== --> */}
 
-                {/* <!-- ===== Content Area Start ===== --> */}
                 <div className="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto">
-                    {/* <!-- ===== Header Start ===== --> */}
                     <Header
                         sidebarOpen={sidebarOpen}
                         setSidebarOpen={setSidebarOpen}
                     />
-                    {/* <!-- ===== Header End ===== --> */}
 
-                    {/* <!-- ===== Main Content Start ===== --> */}
                     <main>
                         <div className="p-4 mx-auto max-w-screen-2xl md:p-6 2xl:p-10">
                             {children}
                         </div>
                     </main>
-                    {/* <!-- ===== Main Content End ===== --> */}
                 </div>
-                {/* <!-- ===== Content Area End ===== --> */}
             </div>
-            {/* <!-- ===== Page Wrapper End ===== --> */}
         </div>
     );
 };
