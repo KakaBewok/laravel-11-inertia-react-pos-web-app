9e99d1af3c3ec08233fe1427c7b80c566e9cabab
noprizal <noprizal@voltras.co.id>
2024-10-06 16:42:24 +0700

    fixing profile page

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -3,20 +3,21 @@ import { PageProps } from "@/types";
 import DeleteUserForm from "./Partials/DeleteUserForm";
 import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
 import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm";
+import { Heading } from "@/Components/ui/heading";
 
 export default function Edit({
     mustVerifyEmail,
     status,
 }: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
     return (
-        <AuthenticatedLayout
-        // header={<h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">Profile</h2>}
-        >
-            {/* <Head title="Profile" /> */}
-
-            <div className="py-12">
+        <AuthenticatedLayout>
+            <div className="py-8">
                 <div className="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
-                    <div className="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+                    <Heading
+                        title="Edit Profile"
+                        description="Manage your profile"
+                    />
+                    <div className="p-4 bg-white shadow-md sm:p-8 dark:bg-gray-700 sm:rounded-lg">
                         <UpdateProfileInformationForm
                             mustVerifyEmail={mustVerifyEmail}
                             status={status}
@@ -24,11 +25,11 @@ export default function Edit({
                         />
                     </div>
 
-                    <div className="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+                    <div className="p-4 bg-white shadow-md sm:p-8 dark:bg-gray-700 sm:rounded-lg">
                         <UpdatePasswordForm className="max-w-xl" />
                     </div>
 
-                    <div className="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+                    <div className="p-4 bg-white shadow-md sm:p-8 dark:bg-gray-700 sm:rounded-lg">
                         <DeleteUserForm className="max-w-xl" />
                     </div>
                 </div>
