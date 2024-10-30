d4d7ec887432e2b70329826ed60d6481d260843c
Noprizal <noprizal@voltras.co.id>
2024-10-30 17:20:15 +0700

    setup create order

--- resources/js/Pages/Order/create.tsx
+++ resources/js/Pages/Order/create.tsx
@@ -1,17 +1,26 @@
-import Category from "@/interfaces/Category";
+import PaymentMethod from "@/interfaces/PaymentMethod";
+import Product from "@/interfaces/Product";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import MainLayout from "@/Layouts/MainLayout";
 import { Head } from "@inertiajs/react";
-import { OrderForm } from "./components/order-form";
+// import { OrderForm } from "./components/order-form";
 
-const CreatePage = ({ categories }: { categories: Category[] }) => {
+interface CreatePageProps {
+    paymentMethods: PaymentMethod[];
+    products: Product[];
+}
+
+const CreatePage = ({ paymentMethods, products }: CreatePageProps) => {
     return (
         <MainLayout>
             <AuthenticatedLayout>
                 <Head title="Create order" />
                 <div className="flex-col">
                     <div className="flex-1 p-4 pt-5 space-y-4 md:p-8">
-                        <OrderForm categories={categories} />
+                        {/* <OrderForm
+                            paymentMethods={paymentMethods}
+                            products={products}
+                        /> */}
                     </div>
                 </div>
             </AuthenticatedLayout>
