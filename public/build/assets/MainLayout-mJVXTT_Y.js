55d0f2d2cf3971a1d8487217e49da68fcb28d3a1
Noprizal <noprizal@voltras.co.id>
2024-11-08 13:55:19 +0700

    fixc

---
+++ public/build/assets/MainLayout-mJVXTT_Y.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-BmY1Do-R.js";import{G as r}from"./useGlobalContext-DEDc1K2H.js";import{Q as s}from"./react-toastify.esm-DQOW_WDH.js";const a=({children:t})=>o.jsx(r,{children:o.jsxs("div",{className:"main-layout",children:[o.jsx(s,{autoClose:3100}),t]})}),m=a;export{m as M};
