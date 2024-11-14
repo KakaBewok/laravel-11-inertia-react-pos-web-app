8b77a420c8d36fdb2a7b698abe909fb6e9cdf91d
Noprizal <noprizal@voltras.co.id>
2024-11-14 19:00:19 +0700

    create order

--- public/build/assets/MainLayout-Dm35CpZE.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-CBjwQAxT.js";import{G as r}from"./useGlobalContext-WPfucgOM.js";import{Q as s}from"./react-toastify.esm-CFj9e9l2.js";const a=({children:t})=>o.jsx(r,{children:o.jsxs("div",{className:"main-layout",children:[o.jsx(s,{autoClose:3100}),t]})}),m=a;export{m as M};
