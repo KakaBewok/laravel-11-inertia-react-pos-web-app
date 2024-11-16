db65e08b6244f82337f2d328c680024b05b7d220
noprizal <rizalnov667@gmail.com>
2024-11-16 20:38:23 +0700

    fix

--- public/build/assets/MainLayout-D1x_gnGm.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-D9pFrhhQ.js";import{G as r}from"./useGlobalContext-D4zjIaB6.js";import{Q as s}from"./react-toastify.esm-DoOGIfX5.js";const a=({children:t})=>o.jsx(r,{children:o.jsxs("div",{className:"main-layout",children:[o.jsx(s,{autoClose:3100}),t]})}),m=a;export{m as M};
